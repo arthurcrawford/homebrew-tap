5cf953aa93c52f854ebecc7aad93542120deebf4
art <arthur.crawford@hogarthww.com>
2016-04-18 08:23:48 +0100

    Initial commit

---
+++ spingo.rb
@@ -0,0 +1,13 @@
+
+class Spingo < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/arthurcrawford/spingo/archive/1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "e67d9aee4c12ed4009dd746a176c83d5327ec84bb405838d836d1972c8ee0b9d"
+
+  def install
+    bin.install "spingo"
+  end
+
+end
