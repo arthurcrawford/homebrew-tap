8a5417be655b9338a8117868dc111ba324a5d2a1
Arthur Crawford <arthur.crawford@gmail.com>
2023-06-20 18:02:26 +0100

    new version

--- coppernob.rb
+++ coppernob.rb
@@ -3,9 +3,11 @@ class Coppernob < Formula
   include Language::Python::Virtualenv
   desc ""
   homepage ""
-  url "https://github.com/arthurcrawford/coppernob/archive/refs/tags/1.0.tar.gz"
-  version "1.0"
-  sha256 "69efa882e63cee8f694b9d1a4fa8a65b873884ce6b10fb34ce83f6f98cc6b412"
+  url "https://github.com/arthurcrawford/coppernob/archive/refs/tags/1.1.tar.gz"
+  version "1.1"
+  sha256 "1512651c023b162551517a2689270e0672324a6039a4fe293e27b610ef687e7c"
+
+  depends_on "python@3.11"
 
   resource "click" do
     url "https://files.pythonhosted.org/packages/59/87/84326af34517fca8c58418d148f2403df25303e02736832403587318e9e8/click-8.1.3.tar.gz"
@@ -13,7 +15,7 @@ class Coppernob < Formula
   end
 
   def install
-    virtualenv_install_with_resources(:using => "python@3")
+    virtualenv_install_with_resources()
   end
 
 end
